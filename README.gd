a575881e749ad752bba604a8595464b2e1c43395
RuzGhandilian <159611923+RuzGhandilian@users.noreply.github.com>
2024-03-26 00:57:13 +0400

    Initial commit

---
+++ README.gd
@@ -0,0 +1,33 @@
+# Implementation of Linear Regression and Logistic Regression with Backpropagation
+
+## Description
+
+This is a Homework project. The assignment was to implement Linear Regression and Logistic Regression models from scratch using backpropagation for my assignment in Advanced Machine Learning course in Armenian code academy.
+
+### Mathematical Operations
+
+The backpropagation for the following mathematical operations/functions is implemented:
+
+- **Addition**
+- **Multiplication**
+- **Exponentiation**
+- **Sigmoid**
+
+### Linear Regression
+
+The Linear Regression model is constructed using the mathematical operations implemented above. It is trained on a provided dataset.
+
+### Logistic Regression
+
+Similarly, the Logistic Regression model is constructed using the mathematical operations implemented. It is trained on a provided dataset.
+
+## Datasets
+
+Two datasets are used for training:
+
+1. **Breast Cancer Dataset**
+
+2. **Diabetes Dataset**
+
+Both datasets are split into training and testing sets using a test size of 20% and a random state of 42.
+
